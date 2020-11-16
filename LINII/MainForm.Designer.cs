56ed51571b3066b1a1f82e921746457edd5b58d3
fo-xi <dyagaay@gmail.com>
2020-11-16 21:48:25 +0700

    Изменения

---
+++ LINII/MainForm.Designer.cs
@@ -0,0 +1,429 @@
+﻿
+namespace LINII
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.OKButton = new System.Windows.Forms.Button();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.SecondYTextBox = new System.Windows.Forms.TextBox();
+			this.SecondXTextBox = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.FirstYTextBox = new System.Windows.Forms.TextBox();
+			this.FirstXTextBox = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.ClearButton = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.DDARadioButton = new System.Windows.Forms.RadioButton();
+			this.PictureBox = new System.Windows.Forms.PictureBox();
+			this.printDialog1 = new System.Windows.Forms.PrintDialog();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label7 = new System.Windows.Forms.Label();
+			this.ColorsComboBox = new System.Windows.Forms.ComboBox();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.DottedLineStepTextBox = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.DottedLineRadioButton = new System.Windows.Forms.RadioButton();
+			this.SolidLineRadioButton = new System.Windows.Forms.RadioButton();
+			this.ThinLineRadioButton = new System.Windows.Forms.RadioButton();
+			this.label5 = new System.Windows.Forms.Label();
+			this.ThickLineRadioButton = new System.Windows.Forms.RadioButton();
+			this.WidthTextBox = new System.Windows.Forms.TextBox();
+			this.panel1.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+			this.panel2.SuspendLayout();
+			this.groupBox4.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.OKButton);
+			this.panel1.Controls.Add(this.groupBox3);
+			this.panel1.Controls.Add(this.groupBox2);
+			this.panel1.Controls.Add(this.ClearButton);
+			this.panel1.Controls.Add(this.groupBox1);
+			this.panel1.Location = new System.Drawing.Point(386, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(193, 363);
+			this.panel1.TabIndex = 0;
+			// 
+			// OKButton
+			// 
+			this.OKButton.Location = new System.Drawing.Point(113, 277);
+			this.OKButton.Name = "OKButton";
+			this.OKButton.Size = new System.Drawing.Size(75, 23);
+			this.OKButton.TabIndex = 6;
+			this.OKButton.Text = "OK";
+			this.OKButton.UseVisualStyleBackColor = true;
+			this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.SecondYTextBox);
+			this.groupBox3.Controls.Add(this.SecondXTextBox);
+			this.groupBox3.Controls.Add(this.label3);
+			this.groupBox3.Controls.Add(this.label4);
+			this.groupBox3.Location = new System.Drawing.Point(3, 195);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(185, 76);
+			this.groupBox3.TabIndex = 3;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Координаты второй точки";
+			// 
+			// SecondYTextBox
+			// 
+			this.SecondYTextBox.Location = new System.Drawing.Point(27, 44);
+			this.SecondYTextBox.Name = "SecondYTextBox";
+			this.SecondYTextBox.Size = new System.Drawing.Size(152, 20);
+			this.SecondYTextBox.TabIndex = 3;
+			// 
+			// SecondXTextBox
+			// 
+			this.SecondXTextBox.Location = new System.Drawing.Point(27, 19);
+			this.SecondXTextBox.Name = "SecondXTextBox";
+			this.SecondXTextBox.Size = new System.Drawing.Size(152, 20);
+			this.SecondXTextBox.TabIndex = 2;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(6, 47);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(15, 13);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "y:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(6, 25);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(15, 13);
+			this.label4.TabIndex = 0;
+			this.label4.Text = "x:";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.FirstYTextBox);
+			this.groupBox2.Controls.Add(this.FirstXTextBox);
+			this.groupBox2.Controls.Add(this.label2);
+			this.groupBox2.Controls.Add(this.label1);
+			this.groupBox2.Location = new System.Drawing.Point(3, 113);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(185, 76);
+			this.groupBox2.TabIndex = 2;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Координаты первой точки";
+			// 
+			// FirstYTextBox
+			// 
+			this.FirstYTextBox.Location = new System.Drawing.Point(27, 44);
+			this.FirstYTextBox.Name = "FirstYTextBox";
+			this.FirstYTextBox.Size = new System.Drawing.Size(152, 20);
+			this.FirstYTextBox.TabIndex = 3;
+			// 
+			// FirstXTextBox
+			// 
+			this.FirstXTextBox.Location = new System.Drawing.Point(27, 19);
+			this.FirstXTextBox.Name = "FirstXTextBox";
+			this.FirstXTextBox.Size = new System.Drawing.Size(152, 20);
+			this.FirstXTextBox.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 47);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(15, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "y:";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 25);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(15, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "x:";
+			// 
+			// ClearButton
+			// 
+			this.ClearButton.Location = new System.Drawing.Point(113, 335);
+			this.ClearButton.Name = "ClearButton";
+			this.ClearButton.Size = new System.Drawing.Size(75, 23);
+			this.ClearButton.TabIndex = 1;
+			this.ClearButton.Text = "Очистить";
+			this.ClearButton.UseVisualStyleBackColor = true;
+			this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Controls.Add(this.DDARadioButton);
+			this.groupBox1.Location = new System.Drawing.Point(3, 3);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(185, 104);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Выберите алгоритм";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(6, 65);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(99, 17);
+			this.radioButton1.TabIndex = 1;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Обычный ЦДА";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// DDARadioButton
+			// 
+			this.DDARadioButton.AutoSize = true;
+			this.DDARadioButton.Location = new System.Drawing.Point(6, 30);
+			this.DDARadioButton.Name = "DDARadioButton";
+			this.DDARadioButton.Size = new System.Drawing.Size(99, 17);
+			this.DDARadioButton.TabIndex = 0;
+			this.DDARadioButton.TabStop = true;
+			this.DDARadioButton.Text = "Обычный ЦДА";
+			this.DDARadioButton.UseVisualStyleBackColor = true;
+			// 
+			// PictureBox
+			// 
+			this.PictureBox.Location = new System.Drawing.Point(12, 12);
+			this.PictureBox.Name = "PictureBox";
+			this.PictureBox.Size = new System.Drawing.Size(368, 363);
+			this.PictureBox.TabIndex = 1;
+			this.PictureBox.TabStop = false;
+			this.PictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+			this.PictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+			// 
+			// printDialog1
+			// 
+			this.printDialog1.UseEXDialog = true;
+			// 
+			// panel2
+			// 
+			this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel2.Controls.Add(this.label7);
+			this.panel2.Controls.Add(this.ColorsComboBox);
+			this.panel2.Controls.Add(this.groupBox4);
+			this.panel2.Location = new System.Drawing.Point(585, 12);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(193, 363);
+			this.panel2.TabIndex = 2;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(3, 15);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(35, 13);
+			this.label7.TabIndex = 5;
+			this.label7.Text = "Цвет:";
+			// 
+			// ColorsComboBox
+			// 
+			this.ColorsComboBox.FormattingEnabled = true;
+			this.ColorsComboBox.Location = new System.Drawing.Point(44, 12);
+			this.ColorsComboBox.Name = "ColorsComboBox";
+			this.ColorsComboBox.Size = new System.Drawing.Size(138, 21);
+			this.ColorsComboBox.TabIndex = 4;
+			this.ColorsComboBox.SelectedIndexChanged += new System.EventHandler(this.ColorsComboBox_SelectedIndexChanged);
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.Controls.Add(this.DottedLineStepTextBox);
+			this.groupBox4.Controls.Add(this.label6);
+			this.groupBox4.Controls.Add(this.DottedLineRadioButton);
+			this.groupBox4.Controls.Add(this.SolidLineRadioButton);
+			this.groupBox4.Controls.Add(this.ThinLineRadioButton);
+			this.groupBox4.Controls.Add(this.label5);
+			this.groupBox4.Controls.Add(this.ThickLineRadioButton);
+			this.groupBox4.Controls.Add(this.WidthTextBox);
+			this.groupBox4.Location = new System.Drawing.Point(3, 46);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(185, 187);
+			this.groupBox4.TabIndex = 3;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "Настройка стиля линии";
+			// 
+			// DottedLineStepTextBox
+			// 
+			this.DottedLineStepTextBox.Location = new System.Drawing.Point(91, 146);
+			this.DottedLineStepTextBox.Name = "DottedLineStepTextBox";
+			this.DottedLineStepTextBox.Size = new System.Drawing.Size(88, 20);
+			this.DottedLineStepTextBox.TabIndex = 10;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(6, 149);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(79, 13);
+			this.label6.TabIndex = 9;
+			this.label6.Text = "Шаг пунктира:";
+			// 
+			// DottedLineRadioButton
+			// 
+			this.DottedLineRadioButton.AutoSize = true;
+			this.DottedLineRadioButton.Location = new System.Drawing.Point(6, 121);
+			this.DottedLineRadioButton.Name = "DottedLineRadioButton";
+			this.DottedLineRadioButton.Size = new System.Drawing.Size(118, 17);
+			this.DottedLineRadioButton.TabIndex = 8;
+			this.DottedLineRadioButton.TabStop = true;
+			this.DottedLineRadioButton.Text = "Пунктирная линия";
+			this.DottedLineRadioButton.UseVisualStyleBackColor = true;
+			this.DottedLineRadioButton.CheckedChanged += new System.EventHandler(this.DottedLineRadioButton_CheckedChanged);
+			// 
+			// SolidLineRadioButton
+			// 
+			this.SolidLineRadioButton.AutoSize = true;
+			this.SolidLineRadioButton.Location = new System.Drawing.Point(6, 98);
+			this.SolidLineRadioButton.Name = "SolidLineRadioButton";
+			this.SolidLineRadioButton.Size = new System.Drawing.Size(109, 17);
+			this.SolidLineRadioButton.TabIndex = 7;
+			this.SolidLineRadioButton.TabStop = true;
+			this.SolidLineRadioButton.Text = "Сплошная линия";
+			this.SolidLineRadioButton.UseVisualStyleBackColor = true;
+			this.SolidLineRadioButton.CheckedChanged += new System.EventHandler(this.SolidLineRadioButton_CheckedChanged);
+			// 
+			// ThinLineRadioButton
+			// 
+			this.ThinLineRadioButton.AutoSize = true;
+			this.ThinLineRadioButton.Location = new System.Drawing.Point(6, 75);
+			this.ThinLineRadioButton.Name = "ThinLineRadioButton";
+			this.ThinLineRadioButton.Size = new System.Drawing.Size(95, 17);
+			this.ThinLineRadioButton.TabIndex = 6;
+			this.ThinLineRadioButton.TabStop = true;
+			this.ThinLineRadioButton.Text = "Тонкая линия";
+			this.ThinLineRadioButton.UseVisualStyleBackColor = true;
+			this.ThinLineRadioButton.CheckedChanged += new System.EventHandler(this.ThinLineRadioButton_CheckedChanged);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(6, 49);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(56, 13);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "Толщина:";
+			// 
+			// ThickLineRadioButton
+			// 
+			this.ThickLineRadioButton.AutoSize = true;
+			this.ThickLineRadioButton.Location = new System.Drawing.Point(6, 22);
+			this.ThickLineRadioButton.Name = "ThickLineRadioButton";
+			this.ThickLineRadioButton.Size = new System.Drawing.Size(100, 17);
+			this.ThickLineRadioButton.TabIndex = 4;
+			this.ThickLineRadioButton.TabStop = true;
+			this.ThickLineRadioButton.Text = "Толстая линия";
+			this.ThickLineRadioButton.UseVisualStyleBackColor = true;
+			this.ThickLineRadioButton.CheckedChanged += new System.EventHandler(this.ThickLineRadioButton_CheckedChanged);
+			// 
+			// WidthTextBox
+			// 
+			this.WidthTextBox.Location = new System.Drawing.Point(68, 46);
+			this.WidthTextBox.Name = "WidthTextBox";
+			this.WidthTextBox.Size = new System.Drawing.Size(111, 20);
+			this.WidthTextBox.TabIndex = 2;
+			this.WidthTextBox.TextChanged += new System.EventHandler(this.WidthTextBox_TextChanged);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(790, 387);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.PictureBox);
+			this.Controls.Add(this.panel1);
+			this.Name = "MainForm";
+			this.Text = "Генерация отрезков";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel1.ResumeLayout(false);
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.groupBox4.ResumeLayout(false);
+			this.groupBox4.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button ClearButton;
+		private System.Windows.Forms.RadioButton DDARadioButton;
+		private System.Windows.Forms.PictureBox PictureBox;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.TextBox SecondYTextBox;
+		private System.Windows.Forms.TextBox SecondXTextBox;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.TextBox FirstYTextBox;
+		private System.Windows.Forms.TextBox FirstXTextBox;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.PrintDialog printDialog1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.TextBox WidthTextBox;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.ComboBox ColorsComboBox;
+		private System.Windows.Forms.TextBox DottedLineStepTextBox;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.RadioButton DottedLineRadioButton;
+		private System.Windows.Forms.RadioButton SolidLineRadioButton;
+		private System.Windows.Forms.RadioButton ThinLineRadioButton;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.RadioButton ThickLineRadioButton;
+		private System.Windows.Forms.Button OKButton;
+	}
+}
+
---
+++ LINII/MainForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace LINII
+{
+	public partial class MainForm : Form
+	{
+		public int xn, yn, xk, yk;
+
+		public Pen pen = new Pen(Color.Black);
+
+		private void ClearButton_Click(object sender, EventArgs e)
+		{
+			PictureBox.Image = null;
+		}
+
+		private void ColorsComboBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			switch (ColorsComboBox.SelectedIndex)
+			{
+				case 0:
+				{
+					pen = new Pen(Color.DarkRed);
+					break;
+				}
+				case 1:
+				{
+					pen = new Pen(Color.PaleVioletRed);
+					break;
+				}
+				case 2:
+				{
+					pen = new Pen(Color.CornflowerBlue);
+					break;
+				}
+				case 3:
+				{
+					pen = new Pen(Color.DarkGreen);
+					break;
+				}
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			List<string> colors = new List<string>{"Red","Pink","Blue","Green"};
+			ColorsComboBox.DataSource = colors;
+		}
+
+		private void OKButton_Click(object sender, EventArgs e)
+		{
+			xn = Int32.Parse(FirstXTextBox.Text);
+			yn = Int32.Parse(FirstYTextBox.Text);
+
+			xk = Int32.Parse(SecondXTextBox.Text);
+			yk = Int32.Parse(SecondYTextBox.Text);
+
+			DrawByCoordinates(xn, yn, xk, yk);
+		}
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void ThickLineRadioButton_CheckedChanged(object sender, EventArgs e)
+		{
+			WidthTextBox.Enabled = true;
+			if (WidthTextBox.Text != "")
+			{
+				int width = Int32.Parse(WidthTextBox.Text);
+				pen = new Pen(pen.Color, width);
+			}
+		}
+
+		private void WidthTextBox_TextChanged(object sender, EventArgs e)
+		{
+			int width = Int32.Parse(WidthTextBox.Text);
+			pen = new Pen(pen.Color, width);
+		}
+
+		private void ThinLineRadioButton_CheckedChanged(object sender, EventArgs e)
+		{
+			WidthTextBox.Enabled = false;
+			pen = new Pen(pen.Color, 2);
+		}
+
+		private void SolidLineRadioButton_CheckedChanged(object sender, EventArgs e)
+		{
+			DottedLineStepTextBox.Enabled = false;
+		}
+
+		private void DottedLineRadioButton_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (DDARadioButton.Checked == true)
+			{
+				xn = e.X;
+				yn = e.Y;
+			}
+			else
+			{
+				MessageBox.Show("Вы не выбрали алгоритм вывода фигуры!");
+			}
+		}
+
+		private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+		{
+
+			xk = e.X;
+			yk = e.Y;
+
+			DrawByCoordinates(xn, yn, xk, yk);
+
+		}
+
+		private void DrawByCoordinates(int firstPointX, int firstPointY, int secondPointX, int secondPointY)
+		{
+			xk = secondPointX;
+			yk = secondPointY;
+
+			double dx = xk - firstPointX;
+			double dy = yk - firstPointY;
+			//количетсво точек между первый и конечной точкой отрезка
+			int n = 1000;
+			//координаты точек, которые находятся между начальной и конечной точкой
+			double xt = firstPointX;
+			double yt = firstPointY;
+			for (int i = 1; i <= n; i++)
+			{
+				Graphics g = Graphics.FromHwnd(PictureBox.Handle);
+				g.DrawRectangle(pen, (int)xt, (int)yt, 2, 2);
+				xt = xt + dx / n;
+				yt = yt + dy / n;
+			}
+		}
+	}
+}

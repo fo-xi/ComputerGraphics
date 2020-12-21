997f9e84f63d947c87af21120a7a371eadaa1c77
fo-xi <dyagaay@gmail.com>
2020-12-21 21:31:10 +0700

    Изменения

--- OpenGLTest/Примитивы OpenGL.Designer.cs
+++ OpenGLTest/Примитивы OpenGL.Designer.cs
@@ -29,6 +29,7 @@ namespace OpenGLTest
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Pr = new Tao.Platform.Windows.SimpleOpenGlControl();
             this.ConeButton = new System.Windows.Forms.Button();
             this.CubeButton = new System.Windows.Forms.Button();
@@ -45,6 +46,10 @@ namespace OpenGLTest
             this.StartButton = new System.Windows.Forms.Button();
             this.WireRadioButton = new System.Windows.Forms.RadioButton();
             this.SolidRadioButton = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.ColorComboBox = new System.Windows.Forms.ComboBox();
+            this.PointButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Pr
@@ -59,7 +64,7 @@ namespace OpenGLTest
             this.Pr.DepthBits = ((byte)(16));
             this.Pr.Location = new System.Drawing.Point(12, 12);
             this.Pr.Name = "Pr";
-            this.Pr.Size = new System.Drawing.Size(524, 402);
+            this.Pr.Size = new System.Drawing.Size(524, 442);
             this.Pr.StencilBits = ((byte)(0));
             this.Pr.TabIndex = 0;
             // 
@@ -71,6 +76,7 @@ namespace OpenGLTest
             this.ConeButton.TabIndex = 1;
             this.ConeButton.Text = "Cone";
             this.ConeButton.UseVisualStyleBackColor = true;
+            this.ConeButton.Click += new System.EventHandler(this.ConeButton_Click);
             // 
             // CubeButton
             // 
@@ -80,6 +86,7 @@ namespace OpenGLTest
             this.CubeButton.TabIndex = 2;
             this.CubeButton.Text = "Cube";
             this.CubeButton.UseVisualStyleBackColor = true;
+            this.CubeButton.Click += new System.EventHandler(this.CubeButton_Click);
             // 
             // CylinderButton
             // 
@@ -89,6 +96,7 @@ namespace OpenGLTest
             this.CylinderButton.TabIndex = 3;
             this.CylinderButton.Text = "Cylinder";
             this.CylinderButton.UseVisualStyleBackColor = true;
+            this.CylinderButton.Click += new System.EventHandler(this.CylinderButton_Click);
             // 
             // DodecahedronButton
             // 
@@ -98,6 +106,7 @@ namespace OpenGLTest
             this.DodecahedronButton.TabIndex = 4;
             this.DodecahedronButton.Text = "Dodecahedron";
             this.DodecahedronButton.UseVisualStyleBackColor = true;
+            this.DodecahedronButton.Click += new System.EventHandler(this.DodecahedronButton_Click);
             // 
             // IcosahedronButton
             // 
@@ -107,6 +116,7 @@ namespace OpenGLTest
             this.IcosahedronButton.TabIndex = 5;
             this.IcosahedronButton.Text = "Icosahedron";
             this.IcosahedronButton.UseVisualStyleBackColor = true;
+            this.IcosahedronButton.Click += new System.EventHandler(this.IcosahedronButton_Click);
             // 
             // OctahedronButton
             // 
@@ -116,6 +126,7 @@ namespace OpenGLTest
             this.OctahedronButton.TabIndex = 6;
             this.OctahedronButton.Text = "Octahedron";
             this.OctahedronButton.UseVisualStyleBackColor = true;
+            this.OctahedronButton.Click += new System.EventHandler(this.OctahedronButton_Click);
             // 
             // RhombicButton
             // 
@@ -125,6 +136,7 @@ namespace OpenGLTest
             this.RhombicButton.TabIndex = 7;
             this.RhombicButton.Text = "Rhombic";
             this.RhombicButton.UseVisualStyleBackColor = true;
+            this.RhombicButton.Click += new System.EventHandler(this.RhombicButton_Click);
             // 
             // SierpinskiButton
             // 
@@ -134,6 +146,7 @@ namespace OpenGLTest
             this.SierpinskiButton.TabIndex = 8;
             this.SierpinskiButton.Text = "Sierpinski";
             this.SierpinskiButton.UseVisualStyleBackColor = true;
+            this.SierpinskiButton.Click += new System.EventHandler(this.SierpinskiButton_Click);
             // 
             // SphereButton
             // 
@@ -143,6 +156,7 @@ namespace OpenGLTest
             this.SphereButton.TabIndex = 9;
             this.SphereButton.Text = "Sphere";
             this.SphereButton.UseVisualStyleBackColor = true;
+            this.SphereButton.Click += new System.EventHandler(this.SphereButton_Click);
             // 
             // TeapotButton
             // 
@@ -152,6 +166,7 @@ namespace OpenGLTest
             this.TeapotButton.TabIndex = 10;
             this.TeapotButton.Text = "Teapot";
             this.TeapotButton.UseVisualStyleBackColor = true;
+            this.TeapotButton.Click += new System.EventHandler(this.TeapotButton_Click);
             // 
             // TetrahedronButton
             // 
@@ -161,6 +176,7 @@ namespace OpenGLTest
             this.TetrahedronButton.TabIndex = 11;
             this.TetrahedronButton.Text = "Tetrahedron";
             this.TetrahedronButton.UseVisualStyleBackColor = true;
+            this.TetrahedronButton.Click += new System.EventHandler(this.TetrahedronButton_Click);
             // 
             // TorusButton
             // 
@@ -170,6 +186,7 @@ namespace OpenGLTest
             this.TorusButton.TabIndex = 12;
             this.TorusButton.Text = "Torus";
             this.TorusButton.UseVisualStyleBackColor = true;
+            this.TorusButton.Click += new System.EventHandler(this.TorusButton_Click);
             // 
             // StartButton
             // 
@@ -179,6 +196,7 @@ namespace OpenGLTest
             this.StartButton.TabIndex = 13;
             this.StartButton.Text = "Start";
             this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
             // 
             // WireRadioButton
             // 
@@ -202,11 +220,46 @@ namespace OpenGLTest
             this.SolidRadioButton.Text = "Solid";
             this.SolidRadioButton.UseVisualStyleBackColor = true;
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(542, 408);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Color:";
+            // 
+            // ColorComboBox
+            // 
+            this.ColorComboBox.FormattingEnabled = true;
+            this.ColorComboBox.Location = new System.Drawing.Point(582, 405);
+            this.ColorComboBox.Name = "ColorComboBox";
+            this.ColorComboBox.Size = new System.Drawing.Size(206, 21);
+            this.ColorComboBox.TabIndex = 17;
+            this.ColorComboBox.SelectedIndexChanged += new System.EventHandler(this.ColorComboBox_SelectedIndexChanged);
+            // 
+            // PointButton
+            // 
+            this.PointButton.Location = new System.Drawing.Point(542, 432);
+            this.PointButton.Name = "PointButton";
+            this.PointButton.Size = new System.Drawing.Size(246, 23);
+            this.PointButton.TabIndex = 18;
+            this.PointButton.Text = "Move Lighting Point";
+            this.PointButton.UseVisualStyleBackColor = true;
+            this.PointButton.Click += new System.EventHandler(this.PointButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 424);
+            this.ClientSize = new System.Drawing.Size(800, 466);
+            this.Controls.Add(this.PointButton);
+            this.Controls.Add(this.ColorComboBox);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.SolidRadioButton);
             this.Controls.Add(this.WireRadioButton);
             this.Controls.Add(this.StartButton);
@@ -225,6 +278,7 @@ namespace OpenGLTest
             this.Controls.Add(this.Pr);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -248,6 +302,10 @@ namespace OpenGLTest
         private System.Windows.Forms.Button StartButton;
         private System.Windows.Forms.RadioButton WireRadioButton;
         private System.Windows.Forms.RadioButton SolidRadioButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ColorComboBox;
+        private System.Windows.Forms.Button PointButton;
     }
 }
 
--- OpenGLTest/Примитивы OpenGL.cs
+++ OpenGLTest/Примитивы OpenGL.cs
@@ -16,9 +16,389 @@ namespace OpenGLTest
 {
     public partial class Form1 : Form
     {
+        public bool f;
+
+        public float angle = 0;
+
+        public int number = 0;
+
+        public float[] mtClr = { 0, 1, 0, 0 };
+
+        public float[] light_position = { 10, 10, -30, 0 };
+
+        public bool isSelected = true;
+
         public Form1()
         {
             InitializeComponent();
+
+            Pr.InitializeContexts();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Gl.glClearColor(0, 0, 0, 1);
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE |
+                                     Glut.GLUT_DEPTH);
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+
+            List<string> colors = new List<string> { "Yellow", "Pink", "Red", "Blue" };
+            ColorComboBox.DataSource = colors;
+        }
+
+        private void ShowSolid(int obj)
+        {
+            switch (obj)
+            {
+                // Конус
+                case 1:
+                {
+                    Glut.glutSolidCone(0.2, 0.75, 16, 8); 
+                    break; 
+                }
+
+                // Куб
+                case 2:
+                {
+                    Glut.glutSolidCube(0.75);
+                    break;
+                }
+
+                //Цилиндр
+                case 3:
+                {
+                    Glut.glutSolidCylinder(0.2, 0.75, 16, 16);
+                    break;
+                }
+
+                // Додекаэдр
+                case 4:
+                {
+                    Gl.glScaled(0.5, 0.5, 0.5);
+                    Glut.glutSolidDodecahedron();
+                    break;
+                }
+
+                // Икосаэдр
+                case 5:
+                {
+                    Glut.glutSolidIcosahedron();
+                    break;
+                }
+
+                // Октаэдр
+                case 6:
+                {
+                    Glut.glutSolidOctahedron();
+                    break;
+                }
+
+                // Ромбический додекаэдр
+                case 7:
+                {
+                    Glut.glutSolidRhombicDodecahedron();
+                    break;
+                }
+
+                //Фрактал Губка Серпиского
+                case 8:
+                {
+                    double[] offset = {0.0};
+                    Glut.glutSolidSierpinskiSponge(7, offset, 1);
+                    break;
+                }
+
+                // Сфера
+                case 9:
+                {
+                    Glut.glutSolidSphere(0.75, 16, 16);
+                    break;
+                }
+
+                // Чайник
+                case 10:
+                {
+                    Glut.glutSolidTeapot(0.5);
+                    break;
+                }
+
+                // Тетраэдр
+                case 11:
+                {
+                    Gl.glRotated(180, 0, 1, 0);
+                    Glut.glutSolidTetrahedron();
+                    break;
+                }
+
+                // Тор
+                case 12:
+                {
+                    Glut.glutSolidTorus(0.15, 0.65, 16, 16);
+                    break;
+                } 
+            }
+        }
+
+        private void Draw(int obj, float angle = 30)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT | Gl.GL_ACCUM_BUFFER_BIT);
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Gl.glRotated(angle, 0, 1, 0);
+            Gl.glRotated(0, 1, 0, 0);
+
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+            if (WireRadioButton.Checked)
+            {
+                Gl.glColor3f(1, 1, 1);
+
+                switch (obj)
+                {
+                    // Конус
+                    case 1:
+                    {
+                        Glut.glutWireCone(0.2, 0.75, 16, 8);
+                        break;
+                    }
+
+                    // Куб
+                    case 2:
+                    {
+                        Glut.glutWireCube(0.75);
+                        break;
+                    }
+
+                    //Цилиндр
+                    case 3:
+                    {
+                        Glut.glutWireCylinder(0.2, 0.75, 16, 16);
+                        break;
+                    }
+
+                    // Додекаэдр
+                    case 4:
+                    {
+                        Gl.glScaled(0.5, 0.5, 0.5);
+                        Glut.glutWireDodecahedron();
+                        break;
+                    }
+
+                    // Икосаэдр
+                    case 5:
+                    {
+                        Glut.glutWireIcosahedron();
+                        break;
+                    }
+
+                    // Октаэдр
+                    case 6:
+                    {
+                        Glut.glutWireOctahedron();
+                        break;
+                    }
+
+                    //Ромбический додекаэдр
+                    case 7:
+                    {
+                        Glut.glutWireRhombicDodecahedron();
+                        break;
+                    }
+
+                    //Фрактал Губка Серпиского
+                    case 8:
+                    {
+                        double[] offset = {0, 0, 0};
+                        Glut.glutWireSierpinskiSponge(7, offset, 1);
+                        break;
+                    }
+
+                    // Сфера
+                    case 9:
+                    {
+                        Glut.glutWireSphere(0.75, 16, 16);
+                        break;
+                    }
+
+                    // Чайник
+                    case 10:
+                    {
+                        Glut.glutWireTeapot(0.5);
+                        break;
+                    }
+
+                    // Тетраэдр
+                    case 11:
+                    {
+                        Gl.glRotated(180, 0, 1, 0);
+                        Glut.glutWireTetrahedron();
+                        break;
+                    }
+
+                    // Тор
+                    case 12:
+                    {
+                        Glut.glutWireTorus(0.15, 0.65, 16, 16);
+                        break;
+                    } 
+                }
+            }
+            else if (SolidRadioButton.Checked)
+            {
+                float[] lghtClr = { 1, 1, 1, 0 };
+                
+                Gl.glPolygonMode(Gl.GL_FRONT, Gl.GL_FILL);
+                Gl.glShadeModel(Gl.GL_SMOOTH);
+                Gl.glEnable(Gl.GL_LIGHTING);
+                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light_position);
+                Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_AMBIENT, lghtClr); 
+                Gl.glEnable(Gl.GL_LIGHT0);
+                Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, mtClr);
+                ShowSolid(obj);
+                Gl.glDisable(Gl.GL_LIGHTING);
+            }
+            Pr.Invalidate();
+        }
+
+        private void ConeButton_Click(object sender, EventArgs e)
+        {
+            Draw(1);
+            number = 1;
+        }
+
+        private void CubeButton_Click(object sender, EventArgs e)
+        {
+            Draw(2);
+            number = 2;
+        }
+
+        private void CylinderButton_Click(object sender, EventArgs e)
+        {
+            Draw(3);
+            number = 3;
+        }
+
+        private void DodecahedronButton_Click(object sender, EventArgs e)
+        {
+            Draw(4);
+            number = 4;
+        }
+
+        private void IcosahedronButton_Click(object sender, EventArgs e)
+        {
+            Draw(5);
+            number = 5;
+        }
+
+        private void OctahedronButton_Click(object sender, EventArgs e)
+        {
+            Draw(6);
+            number = 6;
+        }
+
+        private void RhombicButton_Click(object sender, EventArgs e)
+        {
+            Draw(7);
+            number = 7;
+        }
+
+        private void SierpinskiButton_Click(object sender, EventArgs e)
+        {
+            Draw(8);
+            number = 8;
+        }
+
+        private void SphereButton_Click(object sender, EventArgs e)
+        {
+            Draw(9);
+            number = 9;
+        }
+
+        private void TeapotButton_Click(object sender, EventArgs e)
+        {
+            Draw(10);
+            number = 10;
+        }
+
+        private void TetrahedronButton_Click(object sender, EventArgs e)
+        {
+            Draw(11);
+            number = 11;
+        }
+
+        private void TorusButton_Click(object sender, EventArgs e)
+        {
+            Draw(12);
+            number = 12;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+
+            StartButton.Text = "Stop";
+            if (f)
+            {
+                timer1.Start();
+            }
+            else
+            {
+                timer1.Stop();
+                StartButton.Text = "Start";
+            }
+            f = !f;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            angle += 10;
+            Draw(number, angle);
+        }
+
+        private void ColorComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (ColorComboBox.SelectedIndex)
+            {
+                case 0:
+                {
+                    mtClr = new float[] { 1, 1, 0, 0 };
+                    break;
+                }
+                case 1:
+                {
+                    mtClr = new float[] { 1, 0, 1, 0 };
+                    break;
+                }
+                case 2:
+                {
+                    mtClr = new float[] { 1, 0, 0, 0 };
+                    break;
+                }
+                case 3:
+                {
+                    mtClr = new float[] { 0, 0, 1, 0 };
+                    break;
+                }
+            }
+
+            Draw(number);
+        }
+
+        private void PointButton_Click(object sender, EventArgs e)
+        {
+            if (isSelected)
+            {
+                light_position = new float[] { -10, -10, 30, 0 };
+            }
+            else
+            {
+                light_position = new float[] { 10, 10, -30, 0 };
+            }
+
+            isSelected = !isSelected;
+            Draw(number);
         }
     }
 }
